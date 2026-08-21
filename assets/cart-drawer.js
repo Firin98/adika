236342bf1e75f93f639a5c877a930315931f478f
Антон Никитинский <antonnikitinskij@Mac-mini-Anton.local>
2026-08-21 09:48:27 +0300

    cart-drawer

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -140,3 +140,73 @@ class CartDrawerItems extends CartItems {
 }
 
 customElements.define('cart-drawer-items', CartDrawerItems);
+
+
+class FreeShippingBar extends HTMLElement {
+  connectedCallback() {
+    subscribe(PUB_SUB_EVENTS.cartUpdate, this._onCartUpdated.bind(this));
+  }
+
+  get threshold() {
+    return parseFloat(this.getAttribute('threshold'));
+  }
+
+  _onCartUpdated(event) {
+    const totalPrice = event.cartData ? event.cartData.total_price : 0;
+    this.style.setProperty('--progress', Math.min(totalPrice / this.threshold, 1));
+  }
+}
+
+customElements.define('free-shipping-bar', FreeShippingBar);
+
+class CartDrawerRecommendations extends HTMLElement {
+  connectedCallback() {
+    this._initializeRecommendations(this.dataset.productId);
+  }
+
+  _initializeRecommendations(productId) {
+    if (this._observer) this._observer.unobserve(this);
+    this._observer = new IntersectionObserver(
+      (entries, observer) => {
+        if (!entries[0].isIntersecting) return;
+        observer.unobserve(this);
+        this._loadRecommendations(productId);
+      },
+      { rootMargin: '0px 0px 400px 0px' }
+    );
+    this._observer.observe(this);
+  }
+
+  _loadRecommendations(productId) {
+    if (!productId) return;
+    fetch(`${this.dataset.url}&product_id=${productId}&section_id=${this.dataset.sectionId}`)
+      .then((response) => response.text())
+      .then((text) => {
+        const html = document.createElement('div');
+        html.innerHTML = text;
+        const recommendations = html.querySelector('cart-drawer-recommendations');
+        if (recommendations?.innerHTML.trim().length) {
+          this.innerHTML = recommendations.innerHTML;
+          this.classList.add('recommendations--loaded');
+          this._initSwiper();
+        }
+      })
+      .catch((e) => console.error(e));
+  }
+
+  _initSwiper() {
+    const swiperEl = this.querySelector('.swiper--cdr');
+    if (!swiperEl || typeof Swiper === 'undefined') return;
+    new Swiper(swiperEl, {
+      slidesPerView: 1.2,
+      freeMode: false,
+      spaceBetween: 15,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+}
+
+customElements.define('cart-drawer-recommendations', CartDrawerRecommendations);
